caf21121b39781d0fbc7ce1eab208684d8e21b47
viorelmarian <viorel.omv@gmail.com>
2017-01-07 14:49:00 +0200

    Interfata
    Pentru interfata am creat o matrice in care voi pune diferite caractere
    astfel incat sa rezulte o imagine. Momentan am creat marginile.

--- Convertor/convertor.cpp
+++ Convertor/convertor.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
+
+void banner(int x)
+{
+    char mat[13][60];
+    int i,j;
+
+    for(i=0;i<13;i++)
+        for(j=0;j<60;j++)
+            mat[i][j]=32;
+    for(i=0;i<13;i++)
+        mat[i][0]=mat[i][59]=-70;
+    for(i=0;i<60;i++)
+        mat[0][i]=mat[12][i]=-51;
+
+    mat[0][0]=-55;
+    mat[0][59]=-69;
+    mat[12][0]=-56;
+    mat[12][59]=-68;
+
+    for(i=0;i<13;i++)
+    {
+        for(j=0;j<60;j++)
+            cout<<mat[i][j];
+        cout<<endl;
+    }
+}
+
 int main()
 {
-
+    int x;
+    banner(x);
 }
